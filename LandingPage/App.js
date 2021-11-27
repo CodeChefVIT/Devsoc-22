46c0be876313eacc31a0caeef3fecbbde36390d5
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2021-11-28 02:30:24 +0530

    New changes

--- LandingPage/App.js
+++ LandingPage/App.js
@@ -1,7 +1,31 @@
 const scrollConatiner = document.querySelector(".main-container");
 
 scrollConatiner.addEventListener("wheel", (e) => {
-    // console.log(e);
     e.preventDefault();
     scrollConatiner.scrollLeft += e.deltaY;
 });
+
+// On scroll animations
+const targets = document.querySelectorAll(".anim1");
+
+options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+};
+
+// observer = new IntersectionObserver((entries) => {
+//     // console.log(entries);
+//     // console.log(entries[0].target);
+//     entries.forEach((entry) => {
+//         if (entry.intersectionRatio > 0) {
+//             entry.target.style.animation = `anim 2s ${entry.target.dataset.delay}  forwards ease-out`;
+//         } else {
+//             entry.target.style.animation = "none";
+//         }
+//     });
+// }, options);
+
+// targets.forEach((target) => {
+//     observer.observe(target);
+// });
